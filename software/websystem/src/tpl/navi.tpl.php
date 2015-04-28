ae2c89eab411b3a6e2c7f6990c9ae2bb1ee54e52
ngandrass <ngandrass@squacu.de>
2015-04-28 22:02:59 +0200

    Responsive Design: Adjusted internal navigation

--- software/websystem/src/tpl/navi.tpl.php
+++ software/websystem/src/tpl/navi.tpl.php
@@ -1,4 +1,5 @@
-<td class="navi_td">
+<!-- Big navi at the left -->
+<td class="navi_td navi_big">
     <a href="<?=domain?>index.php?p=overview"><img src="<?=domain.dir_img?>logo.png" /></a>
     <hr class="navi_spacer"/>
     <div class="navi_logininfo">
@@ -17,4 +18,25 @@
     }
     ?>
 </td>
-<td class="navi_spacer"></td>
+<td class="navi_spacer navi_big"></td>
+
+<!-- Small navi at the top -->
+<span class="navi_small">
+    <span class="navi_small_topbar">
+        <a href="<?=domain?>">
+            <img class="navi_small_title" src="<?=domain.dir_img?>title_split_logo.jpg"/>
+            <img style="max-width: 55%;" class="navi_small_title" src="<?=domain.dir_img?>title_split_text.jpg"/>
+        </a>
+        <a class="navi_small_opener" style="color:#ffffff;" href="#"><img src="<?=domain.dir_img?>menu.png"/></a><br/>
+    </span>
+    <span class="navi_small_menu">
+        <?php
+        switch($sessions->getUserLevel()) {
+            case 1: require(dir_navis."navi_user.tpl.php"); break;
+            case 2: require(dir_navis."navi_mod.tpl.php"); break;
+            case 3: require(dir_navis."navi_admin.tpl.php"); break;
+            default: break;
+        }
+        ?>
+    </span>
+</span>
